5b937a7bbd0e71cce9c9f81fd37edd4675ee255c
rdp1070 <rdp1070@rit.edu>
2016-03-05 18:16:02 -0500

    Add Platform.js

---
+++ game/scripts/Platform.js
@@ -0,0 +1,33 @@
+"use strict"
+
+function makePlatform() {
+	var Platform;
+	return Platform = {
+		x : 0,
+		y : 0,
+		width : 0, 
+		height : 0,
+		_spriteSheet : null,
+		_spriteWidth : 0,
+		_spriteHeight : 0,
+
+
+		/**
+		* updates the game object
+		* @param {player} player - the player object
+		*/
+		update: function(player) {
+
+		}, // end update function
+
+		/**
+		* draws the object to the screen
+		* @param {DrawingContext} ctx - the drawing context on which the collectible will be drawn
+		* @param {int} scale - represents the screen size, allows proper resizing
+		*/
+		draw: function(ctx, scale) {
+
+		}, // end draw function
+
+	}
+}
