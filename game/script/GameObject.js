abcc3a936096a139c97476f943ef9b74d4fac7ce
rdp1070 <rdp1070@rit.edu>
2016-03-05 13:35:40 -0500

    Add GameObject.js
    Includes variables and placeholder functions

---
+++ game/script/GameObject.js
@@ -0,0 +1,32 @@
+"use strict"
+
+function makeGameObject() {
+	var GameObj;
+	return GameObj = {
+		x : 0,
+		y : 0,
+		width : 0, 
+		height : 0,
+		_spriteSheet : ,
+		_spriteWidth : 0,
+		_spriteHeight : 0,
+
+
+		/**
+		* updates the game object
+		* @param {player} player - the player object
+		*/
+		update: function(player) {
+
+		}, // end update function
+
+		/**
+		* draws the object to the screen
+		* @param {int} scale - represents the screen size, allows proper resizing
+		*/
+		draw: function(scale) {
+
+		}, // end draw function
+
+	}
+}
