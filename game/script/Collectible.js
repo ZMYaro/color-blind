b82fd687615851727de8e54dd8327b86dbc6f3fd
rdp1070 <rdp1070@rit.edu>
2016-03-05 14:00:44 -0500

    Add ctx to draw function params
    also add null for _spriteSheet defaults

--- game/script/Collectible.js
+++ game/script/Collectible.js
@@ -7,7 +7,7 @@ function makeCollectible() {
 		y : 0,
 		width : 0, 
 		height : 0,
-		_spriteSheet : ,
+		_spriteSheet : null,
 		_spriteWidth : 0,
 		_spriteHeight : 0,
 
@@ -22,9 +22,10 @@ function makeCollectible() {
 
 		/**
 		* draws the object to the screen
+		* @param {DrawingContext} ctx - the drawing context on which the collectible will be drawn
 		* @param {int} scale - represents the screen size, allows proper resizing
 		*/
-		draw: function(scale) {
+		draw: function(ctx, scale) {
 
 		}, // end draw function
 
