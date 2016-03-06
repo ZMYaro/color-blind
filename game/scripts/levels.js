eb769ae9a40504c1ac7238095dc99bea2f0676ba
rdp1070 <rdp1070@rit.edu>
2016-03-06 00:02:45 -0500

    Make Level 1 more interesting

--- game/scripts/levels.js
+++ game/scripts/levels.js
@@ -2,8 +2,8 @@
 var LEVELS = [
 	{
 		"name": "Level 1: The Startening",
-		"width": 300,
-		"height": 300,
+		"width": 1000,
+		"height": 1000,
 		"bgImage": "images/bgs/testBg.png",
 		"playerStart": {
 			"x": 0,
@@ -12,14 +12,32 @@ var LEVELS = [
 		"objects": [{
 			"type": "platform",
 			"x": 0,
-			"y": 2,
-			"width": 4,
-			"height": 1,
+			"y": 100,
+			"width": 100,
+			"height": 100,
 			"color": "#ffff00"
 		}, {
 			"type": "bolt",
 			"x": 2,
 			"y": 1,
+			"width": 2,
+			"height": 2
+		}, {
+			"type": "bolt",
+			"x": 30,
+			"y": 1,
+			"width": 2,
+			"height": 2
+		}, {
+			"type": "bolt",
+			"x": 30,
+			"y": 100,
+			"width": 2,
+			"height": 2
+		}, {
+			"type": "bolt",
+			"x": 300,
+			"y": 100,
 			"width": 2,
 			"height": 2
 		}]
