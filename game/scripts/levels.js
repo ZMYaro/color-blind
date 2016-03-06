788675e2360d8f9b477b21885e863d2ceea069d9
rdp1070 <rdp1070@rit.edu>
2016-03-06 02:37:20 -0500

    Change Level 1

--- game/scripts/levels.js
+++ game/scripts/levels.js
@@ -2,35 +2,71 @@
 var LEVELS = [
 	{
 		"name": "Level 1: The Startening",
-		"width": 200,
-		"height": 16,
+		"width": 600,
+		"height": 50,
 		"bgImage": "images/bgs/testBg.png",
 		"playerStart": {
-			"x": 0,
-			"y": 0
+			"x": 2,
+			"y": 8
 		},
 		"objects": [{
 			"type": "platform",
 			"x": 0,
-			"y": 12,
-			"width": 200,
+			"y": 16,
+			"width": 32,
 			"height": 4,
 			"color": "#ffff00"
 		}, {
 			"type": "platform",
-			"x": 2,
-			"y": 11,
-			"width": 2,
-			"height": 1,
+			"x": 0,
+			"y": 0,
+			"width": 1,
+			"height": 16,
 			"color": "#ffff00"
 		}, {
 			"type": "platform",
-			"x": 8,
+			"x": 20,
 			"y": 8,
 			"width": 4,
 			"height": 1,
 			"color": "#ffff00"
 		}, {
+			"type": "platform",
+			"x": 22,
+			"y": 12,
+			"width": 10,
+			"height": 4,
+			"color": "#ffff00"
+		}, {
+			"type": "platform",
+			"x": 27,
+			"y": 6,
+			"width": 5,
+			"height": 10,
+			"color": "#ffff00"
+		}, {
+			"type": "platform",
+			"x": 35,
+			"y": 12,
+			"width": 30,
+			"height": 4,
+			"color": "#ffff00"
+		}, {
+			"type": "platform",
+			"x": 40,
+			"y": 8,
+			"width": 30,
+			"height": 4,
+			"color": "#ffff00"
+		}, 
+		{
+			"type": "platform",
+			"x": 1,
+			"y": 5,
+			"width": 16,
+			"height": 1,
+			"color": "#ffff00"
+		}, {
 			"type": "bolt",
 			"x": 2,
 			"y": 1,
@@ -44,16 +80,10 @@ var LEVELS = [
 			"height": 2
 		}, {
 			"type": "bolt",
-			"x": 30,
-			"y": 100,
+			"x": 50,
+			"y": 6,
 			"width": 2,
-			"height": 2
-		}, {
-			"type": "bolt",
-			"x": 300,
-			"y": 100,
-			"width": 2,
-			"height": 2
-		}]
+			"height": 4
+		}]// end of level 1
 	}
 ];
