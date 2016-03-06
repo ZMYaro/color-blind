c2d1323428eadeb54a35f7444f6db96839485f66
Zachary Yaro <zmyaro@zmyaro.com>
2016-03-05 19:34:16 -0500

    Add basic level JSON structure and levels.js

---
+++ game/scripts/levels.js
@@ -0,0 +1,21 @@
+// Level JSON to be read by the level builder.
+var LEVELS = [
+	{
+		"name": "Level 1: The Startening",
+		"playerStart": {
+			"x": 0,
+			"y": 0
+		},
+		"objects": [{
+			"type": "platform",
+			"x": 0,
+			"y": 2,
+			"width": 4,
+			"height": 1
+		}, {
+			"type": "bolt",
+			"x": 2,
+			"y": 1
+		}]
+	}
+];
