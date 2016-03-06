b33b3a04cb1f11a95c5cc1726be2afe252dc19be
Zachary Yaro <zmyaro@zmyaro.com>
2016-03-05 22:15:09 -0500

    Fix syntax errors in Platform.draw

--- game/scripts/Platform.js
+++ game/scripts/Platform.js
@@ -21,13 +21,14 @@ var Platform = (function() {
 	 * Draw the platform to the game canvas.
 	 * @param {CanvasRenderingContext2D} ctx - the drawing context on which the collectible will be drawn
 	 * @param {Number} scaleFactor - The ratio of pixel to game grid square
+	 * @override
 	 */
-	draw: function(ctx, scaleFactor) {
+	Platform.prototype.draw = function (ctx, scaleFactor) {
 		ctx.save();
 		ctx.fillStyle(color);
-		ctx.fillRect(x * scaleFactor y * scaleFactor width * scaleFactor height * scaleFactor);
+		ctx.fillRect(x * scaleFactor, y * scaleFactor, width * scaleFactor, height * scaleFactor);
 		ctx.restore();
-	}
+	};
 	
 	return Platform;
 })();
