26bb13547cf122faaa18eb85bb43a08905951c76
Zachary Yaro <zmyaro@zmyaro.com>
2016-03-05 20:16:23 -0500

    Refactor Game to be a class

--- game/scripts/Game.js
+++ game/scripts/Game.js
@@ -1,37 +1,70 @@
-"use strict"
-
-var scaleFactor;
-var ctx;
-var canvas;
-var _player;
-
-
-function Game(canvas_) {
+var Game = (function () {
+	'use strict';
 	
-	canvas = canvas_;
-	ctx = canvas.getContext("2d");
+	/**
+	 * Create a new Game instance.
+	 * @param {HTMLCanvasElement} canvas - The game canvas
+	 */
+	function Game(canvas) {
+		// Initialize the canvas.
+		this.canvas = canvas;
+		this.ctx = canvas.getContext("2d");
+		
+		// Initialize the scale factor, which will be overwritten upon first level load.
+		this._scaleFactor = 1;
+		
+		// Initialize game-related objects.
+		this._levelHeight = 1;
+		//this._player = new Player();
+		this._gameObjects = [];
+		
+		return this;
+	}
 
-	//_player = makePlayer();
-	//need to get scaleFactor
-	//need to generate gameObjects based on 
-}
-
-function loadLevel() {
-
-}
-
-function pause() {
-
-}
-
-function resume() {
-
-}
-
-function _draw() {
-
-}
-
-function _update() {
-
-}
+	Game.prototype = {
+		/**
+		 * Load a given level based on a given level JSON.
+		 * @param {Object} levelJSON
+		 */
+		loadLevel: function (levelJSON) {
+			// TODO
+		},
+		
+		/**
+		 * Suspend gameplay and remove event listeners.
+		 */
+		pause: function () {
+			// TODO
+		},
+		
+		/**
+		 * Re-add event listeners and resume gameplay.
+		 */
+		resume: function () {
+			// TODO
+		},
+		
+		/**
+		 * Recompute scale factor.
+		 */
+		resize: function () {
+			// TODO
+		},
+		
+		/**
+		 * Update game objects.
+		 */
+		_update: function () {
+			// TODO
+		},
+		
+		/**
+		 * Draw game objects to the canvas.
+		 */
+		_draw: function () {
+			// TODO
+		}
+	};
+	
+	return Game;
+})();
