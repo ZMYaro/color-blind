635a087cbc6630147deb7c0d6889e96482e6e56f
Zachary Yaro <zmyaro@zmyaro.com>
2016-03-06 03:44:03 -0500

    Create image manager

---
+++ game/scripts/imageManager.js
@@ -0,0 +1,52 @@
+'use strict';
+var imageManager = (function () {
+	
+	var IMAGES_DIR = 'images/';
+	
+	var imageManager = {
+		/** {Object<String,Image>} A map of file names to images */
+		_images: {},
+		
+		/**
+		 * Load an image into the map.
+		 * @param {String} fileName - The file name of the image to load
+		 * @param {Function} [callback] - Function to call when the image has loaded
+		 */
+		loadImage: function (fileName, callback) {
+			// If the image was already loaded, call the callback, if any, and exit.
+			if (fileName in this._images) {
+				if (typeof callback === 'function') {
+					callback();
+				}
+				return;
+			}
+			
+			// Create the new Image.
+			var newImage = new Image();
+			
+			// Set the call back, if any.
+			if (typeof callback === 'function') {
+				newImage.onload = callback;
+			}
+			
+			newImage.src = IMAGES_DIR + fileName;
+			
+			this._images[fileName] = newImage;
+		},
+		
+		/**
+		 * Fetch a previously loaded image.
+		 * @param {String} fileName - The file name of the image to retrieve
+		 * @returns {Image}
+		 */
+		getImage: function (fileName) {
+			if (fileName in this._images) {
+				return this._images[fileName];
+			}
+			Console.error('Image \u201c' + fileName + '\u201d was requested without having been loaded.');
+			return;
+		}
+	};
+	
+	return imageManager;
+})();
