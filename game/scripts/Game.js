682edb7ccf40ae5cbd646dc649f9cacd9026a9b0
Zachary Yaro <zmyaro@zmyaro.com>
2016-03-05 20:58:23 -0500

    Implement resizing and part of level loading

--- game/scripts/Game.js
+++ game/scripts/Game.js
@@ -14,9 +14,15 @@ var Game = (function () {
 		this._scaleFactor = 1;
 		
 		// Initialize game-related objects.
+		this._playerStartX = 0;
+		this._playerStartY = 0;
+		this._levelWidth = 1;
 		this._levelHeight = 1;
 		//this._player = new Player();
 		this._gameObjects = [];
+		
+		this._boundResize = this.resize.bind(this);
+		window.addEventListener('resize', this._boundResize, false);
 		
 		return this;
 	}
@@ -27,28 +33,50 @@ var Game = (function () {
 		 * @param {Object} levelJSON
 		 */
 		loadLevel: function (levelJSON) {
-			// TODO
+			this._levelWidth = levelJSON.width;
+			this._levelHeight = levelJSON.height;
+			this._playerStartX = levelJSON.playerStart.x;
+			this._playerStartY = levelJSON.playerStart.y;
+			
+			this._gameObjects = [];
+			
+			var that = this;
+			levelJSON.objects.forEach(function (objDef) {
+				var newObj;
+				switch (objDef.type) {
+					case 'platform':
+						// TODO
+						break;
+					case 'bolt':
+						// TODO
+						break;
+				}
+				that._gameObjects.push(newObj);
+			});
+			
+			// Update the scale factor for the new level.
+			this.resize();
 		},
 		
 		/**
 		 * Suspend gameplay and remove event listeners.
 		 */
 		pause: function () {
-			// TODO
+			window.removeEventListener('resize', this._boundResize, false);
 		},
 		
 		/**
 		 * Re-add event listeners and resume gameplay.
 		 */
 		resume: function () {
-			// TODO
+			window.addEventListener('resize', this._boundResize, false);
 		},
 		
 		/**
 		 * Recompute scale factor.
 		 */
 		resize: function () {
-			// TODO
+			this._scaleFactor = window.innerHeight / this._gameHeight;
 		},
 		
 		/**
